a0b2dc10372a8f726e6ef6eb719d586135841070
HyoseonJegal <dott.goo@gmail.com>
2020-03-23 16:31:16 +0900

    주석 정리

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,18 @@ import { Server } from "./app";
 import { hospitalAPI } from './utils/hospitalApi.util'
 import AWS from "aws-sdk";
 AWS.config.update({ region: 'ap-northeast-2' });
-console.log("꽉");
 (async function () {
   try {
 
     const credentials: AWS.SharedIniFileCredentials = new AWS.SharedIniFileCredentials();
-    AWS.config.credentials = credentials; // ~/.aws/credentials 에 저장된 인증 정보 가져옴.
+    // ~/.aws/credentials 에 저장된 인증 정보 가져옴
+    AWS.config.credentials = credentials; 
     const ssm: AWS.SSM = await new AWS.SSM();
 
-    const params: any = { // Request
-      Name: 'config', // 정보를 얻고싶은 parameter 이름.
+    // Request
+    // 정보를 얻고싶은 parameter 이름
+    const params: any = { 
+      Name: 'config', 
       WithDecryption: false
     };
 
