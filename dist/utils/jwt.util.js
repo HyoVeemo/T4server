b9a3105189a6ea8f00777365eaeb73e71db64935
HyoseonJegal <dott.goo@gmail.com>
2020-04-03 17:29:39 +0900

    fixed untracked files

--- dist/utils/jwt.util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jwtToken;
-(function (jwtToken) {
-    jwtToken.secret = 'MySecret';
-    jwtToken.algorithm = 'HS256';
-})(jwtToken = exports.jwtToken || (exports.jwtToken = {}));
-//# sourceMappingURL=jwt.util.js.map
