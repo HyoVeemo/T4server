2cb0307574d2aeb898f5d5b1fba0bacb59831125
Rat2 <leemagnon@naver.com>
2020-05-27 00:45:14 +0900

    코드 리뷰 및 정리

--- src/router/posts.route.ts
+++ src/router/posts.route.ts
@@ -1,5 +1,4 @@
 import * as express from 'express'
-import { verifyUser } from '../middleware/auth.middleware';
 import { hashtagService } from '../service/hashtag.service';
 import { postsHashtagService } from '../service/postsHashtag.service';
 import { S3Upload, uploadImg } from '../utils/imageUpload.util';
@@ -18,25 +17,23 @@ async function createPosts(req: express.Request, res: express.Response) {
     try {
         let hashtags;
         hashtags = req.body.hashtag.split(',')
-       
 
         //elastic search에 저장
         const client = req.app.locals.client;
-        console.log(client)
         const result = await client.index({
             index: 'posts',
             body: {
-                "title._text":req.body.title,
-                "url._text":req.body.url,
-                "img._text":req.body.img,
-                "hashtag._text":hashtags
+                "title._text": req.body.title,
+                "url._text": req.body.url,
+                "img._text": req.body.img,
+                "hashtag._text": hashtags
             }
         })
 
-        for(const row of hashtags){
+        for (const row of hashtags) {
             //DB에 해시태그 추가하는 함수
             let resultHashtag = await hashtagService.getHashtag(row);
-            if(!resultHashtag){
+            if (!resultHashtag) {
                 resultHashtag = await hashtagService.createHashtag({
                     hashtagName: row
                 })
@@ -47,12 +44,12 @@ async function createPosts(req: express.Request, res: express.Response) {
                 hashtagIndex: resultHashtag.hashtagIndex
             })
         }
-        
+
         res.send({
             success: true,
             statusCode: 200,
             result,
-            message: 'CreatePosts'
+            message: 'createPosts'
         });
     } catch (err) {
         res.send({
@@ -71,25 +68,25 @@ async function createPosts(req: express.Request, res: express.Response) {
  */
 async function listPosts(req, res) {
     const client = req.app.locals.client
-        // 입력 예: Posts?filter=""&pn={"offset":0,"page":1} 
-        let { filter, pn } = req.query;
-        pn = JSON.parse(pn);
-        let params = 
-            {
-                "index": "posts",
-                    "body": {
-                        "from": pn.offset * (pn.page - 1),
-                        "size": pn.offset,
-                        "query": {
-                            "dis_max": {
-                                "queries": [
-                                    {"match":{"title._text.nori": filter }},
-                                    {"match":{"hashtag._text.nori": filter }}
-                                ]
-                            }
-                        }
-                    }
+    // 입력 예: Posts?filter=""&pn={"offset":0,"page":1} 
+    let { filter, pn } = req.query;
+    pn = JSON.parse(pn);
+    let params =
+    {
+        "index": "posts",
+        "body": {
+            "from": pn.offset * (pn.page - 1),
+            "size": pn.offset,
+            "query": {
+                "dis_max": {
+                    "queries": [
+                        { "match": { "title._text.nori": filter } },
+                        { "match": { "hashtag._text.nori": filter } }
+                    ]
                 }
+            }
+        }
+    }
     try {
         const { body } = await client.search(params);
         res.send({
@@ -107,50 +104,6 @@ async function listPosts(req, res) {
         })
     }
 }
-
-/**
- * router: 전체 게시글 조회, 제목으로 검색
- * @param req 
- * @param res 
- */
-async function listPostsByHashtag(req, res) {
-    const client = req.app.locals.client
-        // 입력 예: Posts?filter=""&pn={"offset":0,"page":1} 
-        let { filter, pn } = req.query;
-        pn = JSON.parse(pn);
-        let params = 
-            {
-                "index": "posts",
-                    "body": {
-                        "from": pn.offset * (pn.page - 1),
-                        "size": pn.offset,
-                        "query": {
-                            "dis_max": {
-                                "queries": [
-                                    {"match":{"hashtag._text.nori": filter }}
-                                ]
-                            }
-                        }
-                    }
-                }
-    try {
-        const { body } = await client.search(params);
-        res.send({
-            success: true,
-            result: body.hits.hits,
-            statusCode: 200,
-            message: 'listPosts'
-        });
-    } catch (err) {
-        console.log(err)
-        res.send({
-            success: false,
-            statusCode: 500,
-            message: 'listPosts:500'
-        })
-    }
-}
-
 
 async function deletePosts(req, res) {
     try {
@@ -172,14 +125,14 @@ async function deletePosts(req, res) {
         });
 
     } catch (err) {
-        
+
         console.log(err);
         res.send(
             {
-            success: false,
-            statusCode: 500,
-            message: 'deletePosts'
-        })
+                success: false,
+                statusCode: 500,
+                message: 'deletePosts'
+            })
     }
 }
 
