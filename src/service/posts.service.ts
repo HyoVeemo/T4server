6b3ef2ac0d728d769eac788a8b7ae24c1a6c7bfe
Rat2 <leemagnon@naver.com>
2020-05-28 17:04:47 +0900

    listPosts 삭제

--- src/service/posts.service.ts
+++ src/service/posts.service.ts
@@ -1,85 +1,71 @@
 import { Op } from 'sequelize'
 import Posts from '../models/Posts.model'
 
-interface ICreatePostsData{ 
+interface ICreatePostsData {
     title: string,
     url?: string,
-    img?:string, 
+    img?: string,
 }
-interface IUpdatePostsData{
-    title?:string,
-    url?:string,
-    img?:string
+interface IUpdatePostsData {
+    title?: string,
+    url?: string,
+    img?: string
 }
-interface IStorePostsData{
-    postsIndex: number, 
+interface IStorePostsData {
+    postsIndex: number,
     title: string,
-    url?:string,
-    img?:string
+    url?: string,
+    img?: string
 }
 
 class PostsService {
-    constructor(){
+    constructor() {
 
     }
 
-    async createPosts(postsData: ICreatePostsData):Promise<any>{
+    async createPosts(postsData: ICreatePostsData): Promise<any> {
         let resultPosts = await Posts.create(postsData);
         return resultPosts.toJSON();
     }
 
-    async listPostsCount(filter?:any){
+    async listPostsCount(filter?: any) {
         let PostsWhere: any = {};
-        if(filter){
-            if(filter.title){
+        if (filter) {
+            if (filter.title) {
                 PostsWhere.title = filter.title
             }
-            if(filter.content){
+            if (filter.content) {
                 PostsWhere.content = filter.content
             }
         }
         let PostsWhereOption: any = {
             [Op.or]: [
                 {
-                    title: {[Op.like]:PostsWhere.title}
+                    title: { [Op.like]: PostsWhere.title }
                 },
                 {
-                    content: {[Op.like]: PostsWhere.content}
+                    content: { [Op.like]: PostsWhere.content }
                 }
             ]
         }
-    
+
         let options: any = {};
         options.where = PostsWhereOption;
-      
+
         let result = await Posts.findAndCountAll(options);
         return result.count;
     }
-    
-    async listPosts(filter?:any, pn?:any){
-    
-        // const query = `SELECT 
-        // * 
-        // FROM Posts
-        // WHERE title LIKE '%:title%' OR content LIKE '%:content'
-        // ORDER BY createdAt DESC
-        // LIMIT :offset, :limit`
-        // const values = {
-        //     title: filter.title, 
-        //     content: filter.content
-        // }
-    }
 
-    async getPosts(postsIndex: number){
+    async getPosts(postsIndex: number) {
         let result = await Posts.findOne({
             where: {
-                postsIndex:postsIndex
+                postsIndex: postsIndex
             }
         })
         return result;
     }
 
-    async updatePosts(postsData:IUpdatePostsData, postsIndex: number){
+    async updatePosts(postsData: IUpdatePostsData, postsIndex: number) {
         let result = await Posts.update(postsData, {
             where: {
                 postsIndex: postsIndex
@@ -87,7 +73,7 @@ class PostsService {
         })
         return result;
     }
-    async deletePosts(postsIndex: number){
+    async deletePosts(postsIndex: number) {
         let result = await Posts.destroy({
             where: {
                 postsIndex: postsIndex
